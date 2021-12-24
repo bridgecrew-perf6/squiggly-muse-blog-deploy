577941229579af304fcb0e33a8f2042ec5839978
Obaida Zeino <84151630+obaidazeino@users.noreply.github.com>
2021-12-24 06:48:18 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import './media.css';
+import logo from "./components/logo.svg"
+import Navigation from "./components/Navigation"
+import Quotes from "./components/Quotes"
+import data from './data'
+import {Route, Switch, useParams, useLocation} from "react-router-dom"
+import { Page } from './Page'
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 function App() {
+  const location = useLocation()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo-container">
+        <img src={logo} alt="logo"/>
+      </div>
+      <Navigation />
+      <Quotes />
+      <div className="page">
+        {useParams() === {} &&<p>Pick a category</p>}
+        <TransitionGroup>
+        <CSSTransition
+          timeout={300}
+          classNames="transition"
+          key={location.key}
+          >
+        <Switch>
+            <Route path="/:pageId"><Page/></Route>
+            <Route path="/squiggly-muse-blog"><h2 className="pick">Pick a category!</h2></Route>
+        </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+      </div>
+
     </div>
   );
 }
